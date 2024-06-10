0f16fca52f78d45d17b9a57501d0d9cb0c3565c2
Muugii9902 <muugii9902@gmail.com>
2024-06-10 12:52:33 +0800

    js_basic

---
+++ main.js
@@ -0,0 +1,44 @@
+// let ner = "muugii"; //string
+// let nas = 20; //number 20,20.30
+// let gerelsenEseh = true; // boolean: treu/false
+// console.log(ner);
+// //commit
+// console.log("tanii ner: ", ner);
+// console.log("tanii nas: ", nas);
+// console.log("tanii gerleltin baidal: ", gerelsenEseh);
+let fristname = "munkherdene";
+let lastneame = "Ganbaater";
+let country = "ulaanbaatar";
+let email = "muugii9902@gmail.com";
+let age = "30";
+
+console.log("tanii ner: ", fristname);
+console.log("tanii ovog: ", lastneame);
+console.log("tanii nutag: ", country);
+console.log("tanii email: ", email);
+
+let number1 = 1;
+number2 = 2;
+let x = number1 + number2;
+console.log("hariu1", x);
+let y = number1 - number2;
+console.log("hariu2", y);
+let g = number1 * number2;
+console.log("hariu3", g);
+let d = number1 / number2;
+console.log("hariu4", d);
+let a = 12,
+  b = 25,
+  c = 12;
+let p = a + b + c;
+console.log("p", p);
+let gb = 15;
+bt = 1e9;
+let hariu = 15 * 1e9;
+console.log("bt", hariu);
+let celsius = 25;
+let faranheit = (celsius * 9) / 5 + 32;
+console.log("huvirsan", faranheit);
+
+let faranheit1 = 77;
+let celsius1;
